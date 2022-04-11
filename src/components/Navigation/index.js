f2f1d2300ffa1760d158d69dba5403aee3657248
Michael <k1brusenets@gmail.com>
2022-04-11 17:50:24 +0300

    desktop done

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -11,15 +11,51 @@ const Navigation = () => {
     }
     return (
         <nav className='navigation'>
-            <HashLink to ='/#main' smooth scroll={el => scrollWithOffset(el)}  className = 'navigation__link'>ГОЛОВНА</HashLink>
-            <HashLink to ='/#solution' smooth scroll={el => scrollWithOffset(el)}   className = 'navigation__link'>НАШЕ РІШЕННЯ</HashLink>
-            <HashLink to ='/#aboutbot' smooth scroll={el => scrollWithOffset(el)}   className = 'navigation__link'>ПРО ЧАТБОТ</HashLink>
-            <HashLink to ='/#howitwork' smooth scroll={el => scrollWithOffset(el)}   className = 'navigation__link'>ЯК ПРАЦЮЄ</HashLink>
-            <HashLink to ='/#principles' smooth scroll={el => scrollWithOffset(el)}   className = 'navigation__link'>НАШІ ПРИНЦИПИ</HashLink>
-            <HashLink to ='/#partners' smooth scroll={el => scrollWithOffset(el)}   className = 'navigation__link'>ПАРТНЕРИ</HashLink>
-            <HashLink to ='/#faq' smooth scroll={el => scrollWithOffset(el)}   className = 'navigation__link'>FAQ</HashLink>
-            <HashLink to ='/#team' smooth scroll={el => scrollWithOffset(el)}   className = 'navigation__link'>КОМАНДА</HashLink>
-            <HashLink to ='/#advisers' smooth scroll={el => scrollWithOffset(el)}   className = 'navigation__link'>ЕДВАЙЗЕРИ</HashLink>
+            <HashLink to ='/#main' 
+                smooth 
+                scroll={el => scrollWithOffset(el)} 
+                activeclassname = 'selected' 
+                className = 'navigation__link'>ГОЛОВНА</HashLink>
+            <HashLink to ='/#solution' 
+                smooth 
+                scroll={el => scrollWithOffset(el)} 
+                activeclassname = 'selected'  
+                className = 'navigation__link'>НАШЕ РІШЕННЯ</HashLink>
+            <HashLink to ='/#aboutbot' 
+                smooth 
+                scroll={el => scrollWithOffset(el)} 
+                activeclassname = 'selected'  
+                className = 'navigation__link'>ПРО ЧАТБОТ</HashLink>
+            <HashLink to ='/#howitwork' 
+                smooth 
+                scroll={el => scrollWithOffset(el)} 
+                activeclassname = 'selected'  
+                className = 'navigation__link'>ЯК ПРАЦЮЄ</HashLink>
+            <HashLink to ='/#principles' 
+                smooth 
+                scroll={el => scrollWithOffset(el)} 
+                activeclassname = 'selected'  
+                className = 'navigation__link'>НАШІ ПРИНЦИПИ</HashLink>
+            <HashLink to ='/#partners' 
+                smooth 
+                scroll={el => scrollWithOffset(el)} 
+                activeclassname = 'selected'  
+                className = 'navigation__link'>ПАРТНЕРИ</HashLink>
+            <HashLink to ='/#faq' 
+                smooth 
+                scroll={el => scrollWithOffset(el)}  
+                activeclassname = 'selected' 
+                className = 'navigation__link'>FAQ</HashLink>
+            <HashLink to ='/#team' 
+                smooth 
+                scroll={el => scrollWithOffset(el)} 
+                activeclassname = 'selected'  
+                className = 'navigation__link'>КОМАНДА</HashLink>
+            <HashLink to ='/#advisers' 
+                smooth 
+                scroll={el => scrollWithOffset(el)}  
+                activeclassname = 'selected' 
+                className = 'navigation__link'>ЕДВАЙЗЕРИ</HashLink>
         </nav>
     )
 }
