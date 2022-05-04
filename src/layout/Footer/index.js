f98e0fe5c17fd5ed5caaca098d010dc71ec418a3
Michael <k1brusenets@gmail.com>
2022-05-04 19:01:07 +0300

    added right links in footer

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './style.scss';
 import Navigation from '../../components/Navigation';
 import Btn from '../../components/Button';
-import {HashLink} from 'react-router-hash-link';
+import { HashLink } from 'react-router-hash-link';
 import { useTranslation } from "react-i18next";
+import { useLocation } from 'react-router-dom';
 
 const Footer = () => {
+    let path = useLocation().pathname
+    let pathName = (path==='/')? '/' :(path==='ru')?'/ru/': '';
     const { t } = useTranslation();
     return (
         <footer className='container footer'>
@@ -14,9 +17,9 @@ const Footer = () => {
             </div>
             <div className='footer__nav'>
                 <div className='footer__nav-links'>
-                    <HashLink to='/conditions' className='footer__nav-link'>{t('footerConditions')}</HashLink>
-                    <HashLink to='/conditions' className='footer__nav-link'>{t('footerPolitics')}</HashLink>
-                    <HashLink to='/conditions' className='footer__nav-link'>{t('footerCoockies')}</HashLink>
+                    <HashLink to={`${pathName}conditions`} className='footer__nav-link'>{t('footerConditions')}</HashLink>
+                    <HashLink to={`${pathName}conditions`} className='footer__nav-link'>{t('footerPolitics')}</HashLink>
+                    <HashLink to={`${pathName}conditions`} className='footer__nav-link'>{t('footerCoockies')}</HashLink>
                 </div>
                 <Navigation footer />
                 <div className='footer__btn '>
